ab66687a9fbc7828cb50b95dacf64de03e52a1a3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-06-05 02:50:59 +0300

    ComposeApiServer func

--- cmd/server/wire_gen.go
+++ cmd/server/wire_gen.go
@@ -1,7 +1,20 @@
 package main
 
-import (
-	//"fmt"
-	//forums2 "github.com/ILabiak/3lab-kpi/pkg/forums"
-)
+import "github.com/ILabiak/3lab-kpi/pkg/forums"
 
+//"fmt"
+//forums2 "github.com/ILabiak/3lab-kpi/pkg/forums"
+
+func ComposeApiServer(port HttpPortNumber) (*ForumsApiServer, error) {
+	db, err := NewDbConnection()
+	if err != nil {
+		return nil, err
+	}
+	store := forums.NewData(db)
+	httpHandlerFunc := forums.HttpHandler(store)
+	forumsApiServer := &ChatApiServer{
+		Port:          port,
+		ForumsHandler: httpHandlerFunc,
+	}
+	return forumsApiServer, nil
+}
