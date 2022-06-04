efd6e4fb7e90a0303b780778b4a8563740eee539
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-06-05 02:58:06 +0300

    Example client

---
+++ cmd/client/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ILabiak/3lab-kpi/pkg/sdk"
+)
+
+var (
+	target  = flag.String("target", "http://localhost:8080", "Target server address")
+	timeout = flag.Duration("timeout", 5*time.Second, "Timeout for scenarios execution")
+)
+
+func main() {
+	flag.Parse()
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
+
+	client := &sdk.Client{BaseUrl: *target}
+
+	fmt.Println("=== Scenario 1 ===")
+	forums, err := client.ListForums(ctx)
+	if err != nil {
+		log.Fatal("Cannot list forums: ", err)
+	}
+	fmt.Print("Available forums: ")
+	fmt.Println(forums)
+
+	fmt.Println("=== Scenario 2 ===")
+	err = client.CreateUser(ctx, "my-new-channel")
+	if err != nil {
+		log.Fatal("Cannot create channel: ", err)
+	}
+	fmt.Println("Created a new user")
+	forums, err = client.ListForums(ctx)
+	if err != nil {
+		log.Fatal("Cannot list forums: ", err)
+	}
+	fmt.Print("New forums list: ")
+	fmt.Println(forums)
+}
